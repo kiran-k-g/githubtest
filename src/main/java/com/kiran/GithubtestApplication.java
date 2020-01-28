2c9f3009c98e8f51b84632ce4a31518602a8563e
kiran <kirankgpro@gmail.com>
2020-01-28 18:00:10 +0530

    committ

--- src/main/java/com/kiran/GithubtestApplication.java
+++ src/main/java/com/kiran/GithubtestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubtestApplication {
 
-	public static void main(String[] arg) {
-		SpringApplication.run(GithubtestApplication.class, arg);
+	public static void main(String[] args) { 
+		SpringApplication.run(GithubtestApplication.class, args);
 	}
 
 }
